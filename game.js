26ec02abc3c08bd023e41949f59fd0dfbd80534f
juligarc91 <julissagarcia@pursuit.org>
2024-05-10 16:42:04 -0400

    added attack methods

--- game.js
+++ game.js
@@ -5,8 +5,8 @@ class Player {
         this.power = Math.floor(Math.random() * 100) + 50; // can inflict this range of damage onto other object
         this.armor = Math.floor(Math.random() * 21) + 20; // range is 21 numbers (which is 0-20 and adding 20 shifts it to 20-40)
     }
-    attack() {
-        console.log("You were attacked")
+    attack(alienShip) {
+        console.log(`HIT! You've struck an alien ship! Their HP was ${alienShip.hp}, but after you attacked with power ${this.power} damage it diminished their ${alienShip.armor} armor  and ${alienShip.hp} HP . Their HP and armor is now a total of ${(alienShip.armor + alienShip.hp) - this.power} hp`)
     }
 }
 
@@ -16,10 +16,17 @@ class AlienShip {
         this.power = Math.floor(Math.random() * 31) + 30; // can inflict this range of damage onto other object
         this.armor = Math.floor(Math.random() * 21);
     }
-    attack() {
-        console.log("You were attacked")
+    attack(player) {
+        console.log(`You attacked ${player.name}`)
     }
 }
+
+const dogeAlienShip = new AlienShip();
+
+const player1 = new Player("player1")
+
+dogeAlienShip.attack(player1);
+player1.attack(dogeAlienShip);
 
 // Testing purposes -- not part of the game!
 // const player1 = new Player("Callister")
