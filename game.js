55b8de6bf9a1e2bd244744ac850b0b0da0a80da3
AnitaOwen <anitaowen@pursuit.org>
2024-05-10 15:47:48 -0400

    completed lab

--- game.js
+++ game.js
@@ -1,6 +1,27 @@
-class Player {}
+class Player {
+    constructor(name){
+        this.name = name;
+        this.hp = 1000;
+        this.power = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+        this.armor = Math.floor(Math.random() * (40 - 20 + 1)) + 20;
+    }
+    attack(target){
+        target.hp -= (this.power - target.armor) 
+        console.log(`HIT! You've struck an alien ship! Their current HP is now ${target.hp}`)
+    }
+}
 
-class AlienShip {}
+class AlienShip {
+    constructor(){
+        this.hp = 100;
+        this.power = Math.floor(Math.random() * (60 - 30 + 1)) + 30;
+        this.armor = Math.floor(Math.random() * 20) + 1;
+    }
+    attack(target){
+        target.hp -= (this.power - target.armor) 
+        console.log(`Alien ship has struck ${target.name}! Your current HP is now ${target.hp}`)
+    }
+}
 
 // Testing purposes -- not part of the game!
 // const player1 = new Player("Callister")
@@ -17,6 +38,39 @@ class AlienShip {}
 // console.log(player1)
 
 // Game Loop so player/enemy can take a turn attacking each other
-const startGame = () => {}
+const startGame = (name) => {
+
+    const player = new Player(name)
+    let playersTurn = true
+
+    // generates a random integer between 6 and 10
+    const numberOfShips = Math.floor(Math.random() * 5) + 6
+    const alienShips = []
+    for(let i = 0; i < numberOfShips; i++){
+        alienShips.push(new AlienShip())
+    }
+
+    while(player.hp > 0 && alienShips.length !== 0){
+        const currentTarget = alienShips[0]
+        if(playersTurn){
+            playersTurn = !playersTurn
+            player.attack(currentTarget)
+            if(currentTarget.hp <= 0){
+                alienShips.shift()
+            }
+        } else {
+            playersTurn = !playersTurn
+            for(let alien of alienShips){
+                alien.attack(player)
+            }
+        }
+    }
+
+    if(alienShips.length === 0){
+        console.log(`YOU WIN! ${player.name} defeated all alien ships!`)
+    } else {
+        console.log(`YOU LOST! Alien ships have defeated ${player.name}`)
+    }
+}
 
 startGame('Callister')
